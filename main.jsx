a2a5a2c9ea2cf283a8f7c22720ecd851a6fa10db
CHENO1983 <chenn1983@gmail.com>
2026-05-04 17:25:54 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import OpaLanding from './opa-landing.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <OpaLanding />
-  </React.StrictMode>
-)
