be2522e723f0f94d14a217179426b6c6f102aee0
CHENO1983 <chenn1983@gmail.com>
2026-05-04 17:20:35 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OpaLanding from './opa-landing'
+import OpaLanding from './opa-landing.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
